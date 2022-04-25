77eed3d4100ad23924584ecb32ad562cc47ccdcd
Clover <39840298+SmollClover@users.noreply.github.com>
2022-04-25 11:19:28 +0200

    SQL Pseudo Code erstellt

---
+++ Pseudo Code/datenbank.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS JahresAbschlussProjektGruppe1;
+USE JahresAbschlussProjektGruppe1;
+
+CREATE TABLE user (
+    id INT(255) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    registered DATE NOT NULL
+);
+
+CREATE TABLE game (
+    id INT(255) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE score (
+    userid INT(255),
+    gameid INT(255),
+    difficulty INT(255),
+    win INT(255),
+    loss INT(255),
+    PRIMARY KEY(userid, gameid, difficulty)
+);
